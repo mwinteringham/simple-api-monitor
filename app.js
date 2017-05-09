09aec52c038e17adfe355af6c9b03d7a48ebee0c
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2017-05-09 12:09:28 +0100

    Commiting correct code this time

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ var request = require("request");
 
 (function loop() {
   setTimeout(function() {
-    console.log("LOOP");
-    loop();
+    var options = { method: 'GET',
+      url: 'http://localhost:3001/ping'
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      console.log(body);
+
+      loop();
+    });
   }, 1000);
 }());
-
-// 
-// var options = { method: 'GET',
-//   url: 'http://localhost:3001/ping'
-// };
-//
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
-//
-//   console.log(body);
-//   console.log(response.statusCode);
-//
-//   loop();
-// });
